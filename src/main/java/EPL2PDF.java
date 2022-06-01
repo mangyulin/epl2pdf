5bc2c354816e7871e725cdddbcc87bd1cee921d7
mangyulin <mangyulin>
2022-06-01 13:51:41 +0800

    调整面单

--- src/main/java/EPL2PDF.java
+++ src/main/java/EPL2PDF.java
@@ -39,10 +39,22 @@ public class EPL2PDF {
      * @return 返回文件内容vo
      */
 
-    public static List<WordsVo> getWordsVo(File file){
+    public static List<List<WordsVo>> getWordsVo(File file){
         List<String> list = txt2StringArray(file);
-        List<WordsVo> result = new ArrayList<WordsVo>();
+        List<List<WordsVo>> result = new ArrayList<List<WordsVo>>();
+        List<WordsVo> resultWordsList = new ArrayList<WordsVo>();
+        List<String> nn = new ArrayList<String>();
         for (String ss : list) {
+            if("N".equals(ss)){
+                nn.add("N");
+            }
+            //出现一对N为一个面单
+            if(nn.size() == 2){
+                result.add(resultWordsList);
+                nn = new ArrayList<String>();
+                resultWordsList = new ArrayList<WordsVo>();
+                continue;
+            }
             WordsVo wordsVo = new WordsVo();
             String[] s = ss.split(",");
             if (s[0].startsWith("A")){
@@ -55,14 +67,14 @@ public class EPL2PDF {
                 wordsVo.setHeight(Integer.parseInt(s[5]));
                 wordsVo.setCode(s[6]);
                 wordsVo.setData(s[7].replaceAll("\"",""));
-                result.add(wordsVo);
+                resultWordsList.add(wordsVo);
             }else if(s[0].startsWith("LO")){
                 wordsVo.setType("LO");
                 wordsVo.setX(Integer.parseInt(s[0].substring(2)));
                 wordsVo.setY(Integer.parseInt(s[1]));
                 wordsVo.setWide(Integer.parseInt(s[2]));
                 wordsVo.setHeight(Integer.parseInt(s[3]));
-                result.add(wordsVo);
+                resultWordsList.add(wordsVo);
             }else if(s[0].startsWith("B")){
                 wordsVo.setType("B");
                 wordsVo.setX(Integer.parseInt(s[0].substring(2)));
@@ -70,71 +82,86 @@ public class EPL2PDF {
                 wordsVo.setWide(Integer.parseInt(s[2]));
                 wordsVo.setHeight(Integer.parseInt(s[3]));
                 wordsVo.setData(s[8].replaceAll("\"",""));
-                result.add(wordsVo);
+                resultWordsList.add(wordsVo);
             }
         }
         return result;
     }
 
-    public static void main(String[] args){
-        File file = new File("D:\\data\\test.epl");
-        List<WordsVo> list =  getWordsVo(file);
+    public static void EPL2PDF(String path){
+        File file = new File(path);
+        List<List<WordsVo>> list =  getWordsVo(file);
         try {
-            /**
-             * 得到图片缓冲区
-             * INT精确度达到一定,RGB三原色，高度280,宽度360
-             */
-            BufferedImage bi = new BufferedImage(822, 780, BufferedImage.TYPE_BYTE_GRAY);
-
-            //得到它的绘制环境(这张图片的笔)
-            Graphics2D g2 = (Graphics2D) bi.getGraphics();
-
-            //填充一个矩形 左上角坐标(0,0),宽70,高150;填充整张图片
-            g2.fillRect(0, 0, 822, 780);
-            //设置颜色
-            g2.setColor(Color.white);
-            //填充整张图片(其实就是设置背景颜色)
-            g2.fillRect(0, 0, 360, 280);
-            //设置背景颜色
-            g2.setColor(Color.black);
             //添加文字 xy坐标位置
-            for (WordsVo wordsVo : list) {
-                System.out.println(wordsVo);
-                //画文字
-                if("A".equals(wordsVo.getType())){
-                    if(wordsVo.getFontSize() == 1){
-                        //设置字体:字体、字号、大小
-                        g2.setFont(new Font("微软雅黑", Font.PLAIN, 15));
-                    }else if(wordsVo.getFontSize() == 4){
-                        //设置字体:字体、字号、大小
-                        g2.setFont(new Font("微软雅黑", Font.BOLD, 17 * wordsVo.getHeight()));
-                    }
-                    if(wordsVo.getRotation() == 0) {
-                        g2.drawString(wordsVo.getData(), wordsVo.getX() + 5, wordsVo.getY() + wordsVo.getHeight() * 16);
-                    }else if(wordsVo.getRotation() == 1){
-                        g2.rotate((Math.PI/2),wordsVo.getX(), wordsVo.getY());//旋转
-                        g2.drawString(wordsVo.getData(), wordsVo.getX() , wordsVo.getY() + 10);
-                        g2.rotate(-(Math.PI/2),wordsVo.getX(), wordsVo.getY());//旋转
-                    }
-                    //画线
-                }else if("LO".equals(wordsVo.getType())){
-                    //画边框
-                    g2.drawRect(wordsVo.getX(), wordsVo.getY(), wordsVo.getWide(), wordsVo.getHeight());
-                }else if("B".equals(wordsVo.getType())){
-                    //画条码
-                    BufferedImage image =GoogleBarCodeUtils.insertWords(GoogleBarCodeUtils.
-                            getBarCode(wordsVo.getData()),wordsVo.getData().replaceAll("\\%",""));
-//                    ImageIO.write(image, "jpg", new File("d:\\barcode.jpg"));
-                    g2.drawImage(image, wordsVo.getX(), wordsVo.getY(), null);
-                }
-            }
-            //保存图片 JPEG表示保存格式
-            ImageIO.write(bi, "JPEG", new FileOutputStream("d:\\data\\b.jpg"));
+            for (List<WordsVo> listWords : list) {
+                /**
+                 * 得到图片缓冲区
+                 * INT精确度达到一定,RGB三原色，高度280,宽度360
+                 */
+                BufferedImage bi = new BufferedImage(822, 822, BufferedImage.TYPE_BYTE_GRAY);
 
-            System.out.println("成功！");
+                //得到它的绘制环境(这张图片的笔)
+                Graphics2D g2 = (Graphics2D) bi.getGraphics();
+
+                //填充一个矩形 左上角坐标(0,0),宽70,高150;填充整张图片
+                g2.fillRect(0, 0, 822, 822);
+                //设置颜色
+                g2.setColor(Color.white);
+                //填充整张图片(其实就是设置背景颜色)
+                g2.fillRect(0, 0, 360, 280);
+                //设置背景颜色
+                g2.setColor(Color.black);
+                String trackingNo = "";
+                for (WordsVo wordsVo : listWords) {
+                    System.out.println(wordsVo);
+                    if ("A".equals(wordsVo.getType())) {
+                        //判断字号
+                        if (wordsVo.getFontSize() == 1) {
+                            //设置字体:字体、字号、大小
+                            g2.setFont(new Font("微软雅黑", Font.PLAIN, 15));
+                        } else if (wordsVo.getFontSize() == 4) {
+                            //设置字体:字体、字号、大小
+                            g2.setFont(new Font("微软雅黑", Font.BOLD, 17 * wordsVo.getHeight()));
+                        } else {
+                            //设置字体:字体、字号、大小
+                            g2.setFont(new Font("微软雅黑", Font.PLAIN, 15));
+                        }
+                        //画文字
+                        if (wordsVo.getRotation() == 0) {
+                            if (wordsVo.getData().startsWith(" ") && !wordsVo.getData().contains("NEXT")) {
+                                g2.drawString(wordsVo.getData(), wordsVo.getX() + 100, wordsVo.getY() + wordsVo.getHeight() * 16);
+                            } else {
+                                g2.drawString(wordsVo.getData(), wordsVo.getX() + 5, wordsVo.getY() + wordsVo.getHeight() * 16);
+                            }
+                        } else if (wordsVo.getRotation() == 1) {
+                            g2.rotate((Math.PI / 2), wordsVo.getX(), wordsVo.getY());//旋转
+                            g2.drawString(wordsVo.getData(), wordsVo.getX(), wordsVo.getY() + 10);
+                            g2.rotate(-(Math.PI / 2), wordsVo.getX(), wordsVo.getY());//旋转
+                        }
+                        //画线
+                    } else if ("LO".equals(wordsVo.getType())) {
+                        //画边框
+                        g2.drawRect(wordsVo.getX(), wordsVo.getY(), wordsVo.getWide(), wordsVo.getHeight());
+                    } else if ("B".equals(wordsVo.getType())) {
+                        //画条码
+                        BufferedImage image = GoogleBarCodeUtils.
+                                getBarCode(wordsVo.getData());
+                        //                    ImageIO.write(image, "jpg", new File("d:\\barcode.jpg"));
+                        g2.drawImage(image, wordsVo.getX(), wordsVo.getY(), null);
+                        trackingNo = wordsVo.getData();
+                    }
+                }
+                //保存图片 JPEG表示保存格式
+                ImageIO.write(bi, "JPEG", new FileOutputStream("d:\\data\\" + trackingNo + ".jpg"));
+
+                System.out.println("成功！");
+            }
         }catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    public static void main(String[] args){
+        EPL2PDF.EPL2PDF("D:\\data\\test.epl");
 
     }
 }
